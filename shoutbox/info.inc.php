fd39dcacdcfb1fa42cbf03a79c64ae844576132f
André Tarnowsky <lotterfriends@gmail.com>
2013-09-19 08:02:28 +0200

    Update info.inc.php

--- shoutbox/info.inc.php
+++ shoutbox/info.inc.php
@@ -5,18 +5,18 @@
 MODUL -> SHOUTBOX -> INFORMATIONEN
 
 Version 1.0
-(c) 2009 by André Tarnowsy
+(c) 2009 by Andrï¿½ Tarnowsy
 http://www.lotterfriends.net
 
 */
 
-$modul_name		= "Shoutbox";
-$modul_code		= "%SHOUTBOX%";
-$modul_ordner	= "shoutbox";
-$modul_version	= "0.1";
-$modul_author	= "Andr&eacute Tarnowsky";
-$author_url		= "http://www.lotterfriends.net";
-$modul_info		= "Einfache Shoutbox, mit Kommandos,Wortfilter und Smilies.";
-$modul_lizenz	= "http://creativecommons.org/licenses/by-sa/3.0/de/";
+$modul_name    = "Shoutbox";
+$modul_code    = "%SHOUTBOX%";
+$modul_ordner  = "shoutbox";
+$modul_version = "0.1";
+$modul_author  = "Andr&eacute Tarnowsky";
+$author_url    = "http://www.lotterfriends.net";
+$modul_info    = "Einfache Shoutbox, mit Kommandos,Wortfilter und Smilies.";
+$modul_lizenz  = "MIT";
 
 ?>
